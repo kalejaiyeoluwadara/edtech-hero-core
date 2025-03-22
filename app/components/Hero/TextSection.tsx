eef4b26ec6fc5cebf759b5d98d035273d7f6965d
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2025-03-22 16:08:12 +0100

    Enhance TextSection layout with new buttons and scholarship promotion

--- app/components/Hero/TextSection.tsx
+++ app/components/Hero/TextSection.tsx
@@ -2,12 +2,27 @@ import image from '@/public/image'
 import Image from 'next/image'
 import React from 'react'
 import AvatarSection from './AvatarSection'
+import { Icons } from '../ui/icons'
 
 function TextSection() {
   return (
-    <div>
+    <div className='h-full flex flex-col justify-center'>
       <AvatarSection />
-      <h1 className='text-[70px] leading-16 text-white '><span>Online School for</span> <span>Learning <span>Python</span> </span></h1>
+      <h1 className='text-[68px] mt-4 leading-20 text-white '>
+        <span><span className='relative'>Online <span className='absolute -bottom-2 -left-4'><Icons.Line /></span>
+        </span> School for</span><br /> <span>Learning <span className='relative'><span className='relative z-30'>Python</span> <span className='absolute -z-4 left-2  -bottom-2'><Icons.Border /></span> </span></span></h1>
+      <p className='text-white mt-4 '>Learn from experts, build projects, and get certified.</p>
+      <div className='flex mt-6 gap-3'>
+        <button className='bg-[#FBFF48]'>Enroll Now</button>
+        <button className=' border border-[#5C5C5A] text-white '>Learn More</button>
+      </div>
+      <div className='mt-12 w-full relative'>
+        <button className='relative bg-[#231d20e5] px-4 py-2'>
+          <span className='absolute inset-0 border-l-4 border-b-4 border-black'></span>
+          <span className='absolute inset-1 border-l-4 border-b-4 border-[#FBFF48]'></span>
+          <span className='relative text-white flex items-center justify-center'>Win $500 in scholarships</span>
+        </button>
+      </div>
     </div>
   )
 }
